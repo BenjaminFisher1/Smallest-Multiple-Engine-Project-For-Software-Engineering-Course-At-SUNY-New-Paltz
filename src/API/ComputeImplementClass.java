8dd795b62abc6a093081e31dbfb1d706ecb30bdd
timkorug1 <timkorug1@newpaltz.edu>
2025-02-12 12:15:22 -0500

    Take 2
    please

--- src/API/ComputeImplementClass.java
+++ src/API/ComputeImplementClass.java
@@ -1,10 +1,10 @@
-package API;
+package api;
 
 import project.annotations.NetworkAPIPrototype;
 
 @NetworkAPIPrototype
 public abstract class ComputeImplementClass implements ComputeImplement{
-    //Computations in here 
+    //Computations in here
 
 
 }

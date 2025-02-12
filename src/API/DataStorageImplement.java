59e2f4070a1552ab8577d1f47b39732edbef2b44
Ben Fisher <BenLfish00@gmail.com>
2025-02-12 12:42:56 -0500

    removed the overrides

--- src/API/DataStorageImplement.java
+++ src/API/DataStorageImplement.java
@@ -3,13 +3,13 @@ package api;
 @ProcessAPIPrototype
 public class DataStorageImplement implements DataStorage{
 	
-	@Override
+	
 	public int readUserInput(String source) {
 		//read in the user input!!!!!
 		return 1; //example output
 	}
 	
-	@Override
+
 	public void writeOutput(String destination, int computeResult) {
 		//write the computed result to a file
 	}

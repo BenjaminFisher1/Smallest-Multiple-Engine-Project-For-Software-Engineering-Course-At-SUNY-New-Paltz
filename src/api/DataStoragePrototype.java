61609cf97225efbba5189c26209e0dc04d23a6e6
Ben Fisher <BenLfish00@gmail.com>
2025-02-12 14:29:15 -0500

    ARE YOU KIDDING ME
    I MISSED THE LETTER y

--- src/api/DataStoragePrototype.java
+++ src/api/DataStoragePrototype.java
@@ -3,7 +3,7 @@ package api;
 import project.annotations.ProcessAPIPrototype;
 
 
-public class DataStorageProtoype{
+public class DataStoragePrototype{
 	public void prototype(DataStorage dataStorage) {
 		
 		//write user's input to storage so computer can access

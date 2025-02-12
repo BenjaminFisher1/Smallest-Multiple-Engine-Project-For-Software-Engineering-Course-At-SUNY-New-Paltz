cbeb1cd15aba10d2338c6dce301b823a18c7fd13
Ben Fisher <BenLfish00@gmail.com>
2025-02-12 14:47:00 -0500

    created placeholder DataStoreRequest
    Generic wasn't working.

--- src/api/DataStoragePrototype.java
+++ src/api/DataStoragePrototype.java
@@ -6,8 +6,11 @@ import project.annotations.ProcessAPIPrototype;
 public class DataStoragePrototype{
 	public void prototype(DataStorage dataStorage) {
 		
+		//user's input before making it a DataStoreInput object
+		dataStoreRequest userRawInput = new dataStoreRequest();
+		
 		//write user's input to storage so computer can access
-		DataStoreInput userInput = dataStorage.storeData(new dataStoreRequest());
+		DataStoreInput userInput = dataStorage.storeData(userRawInput);
 		
 		//retrieve computed data
 		DataRetrieveComputed computedData = dataStorage.retrieveData(userInput);

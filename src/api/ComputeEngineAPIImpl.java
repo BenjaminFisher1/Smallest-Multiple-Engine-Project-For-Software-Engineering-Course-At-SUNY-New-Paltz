629b2a56bb9144cbddc4954c0368cecfd28ab9a0
Ben Fisher <BenLfish00@gmail.com>
2025-02-12 17:38:46 -0500

    capital letter issue!!!!!!!!!!!!!!
    how silly

--- src/api/ComputeEngineAPIImpl.java
+++ src/api/ComputeEngineAPIImpl.java
@@ -8,7 +8,7 @@ public class ComputeEngineAPIImpl{
 	public void prototype(ComputeEngineAPI computeEngineApi) {
 		ComputeUserInput generalInput = new ComputeUserInput();
 
-	    ProcessJob computeJob = ComputeEngineAPI.computeUponThis(generalInput);
+	    ProcessJob computeJob = computeEngineApi.computeUponThis(generalInput);
 	    
 	}
 	

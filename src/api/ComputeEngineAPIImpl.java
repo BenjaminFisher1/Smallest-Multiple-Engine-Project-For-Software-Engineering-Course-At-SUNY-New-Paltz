a4375d552d240e364bf8393ee7d019a06f81cb47
Ben Fisher <BenLfish00@gmail.com>
2025-02-12 17:33:26 -0500

    Fixed outdated terms

--- src/api/ComputeEngineAPIImpl.java
+++ src/api/ComputeEngineAPIImpl.java
@@ -8,7 +8,7 @@ public class ComputeEngineAPIImpl implements ComputeEngineAPI {
 	public void prototype(ComputeEngineAPI computeEngineApi) {
 		ComputeUserInput generalInput = new ComputeUserInput();
 
-	    ProcessJob computeJob = computeAPI.computeUponThis(generalInput);
+	    ProcessJob computeJob = ComputeEngineAPI.computeUponThis(generalInput);
 	    
 	}
 	

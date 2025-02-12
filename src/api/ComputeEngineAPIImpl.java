39fa27eb83c52d23fae337529b6fc9a4808fe712
Ben Fisher <BenLfish00@gmail.com>
2025-02-12 17:06:06 -0500

    Fixed silly error with having an equals sign equal nothing!!!

--- src/api/ComputeEngineAPIImpl.java
+++ src/api/ComputeEngineAPIImpl.java
@@ -6,7 +6,7 @@ import project.annotations.ConceptualAPIPrototype;
 @ConceptualAPIPrototype
 public class ComputeEngineAPIImpl implements ComputeEngineAPI {
 	ComputeAPIImpl tempComputeAPI = new ComputeAPIImpl();
-    private ComputeAPI computeAPI = ;
+    private ComputeAPI computeAPI = tempComputeAPI ;
 
     @Override
     public int processJob(int n) {

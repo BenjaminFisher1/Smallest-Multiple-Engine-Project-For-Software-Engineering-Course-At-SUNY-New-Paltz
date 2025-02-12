f70acfefc11666e1b6b290fb90542e11f2eea361
Ben Fisher <BenLfish00@gmail.com>
2025-02-12 14:43:46 -0500

    added non null annotation
    this was what she had in her example hopefully itll fix the compile issue

--- src/api/DataStoreRequest.java
+++ src/api/DataStoreRequest.java
@@ -1,5 +1,13 @@
 package api;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+
 public class DataStoreRequest{
 	//request to store this data using DataStoreInput interface
+	
+	//dont know what this does but she had it in her example maybe it will fix this issue
+	@NonNull
+	public String toString() {
+		return "";
+	}
 }

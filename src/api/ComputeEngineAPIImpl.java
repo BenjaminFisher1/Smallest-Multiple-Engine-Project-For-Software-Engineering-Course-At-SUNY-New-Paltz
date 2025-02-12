8f1ffe0a9ab2cb6af6196ae55ff851d210ff017d
timkorug1 <timkorug1@newpaltz.edu>
2025-02-12 16:43:11 -0500

    API #3 !
    hehehehehehe

---
+++ src/api/ComputeEngineAPIImpl.java
@@ -0,0 +1,13 @@
+package api;
+
+import project.annotations.ConceptualAPIPrototype;
+
+@ConceptualAPIPrototype
+public class ComputeEngineAPIImpl implements ComputeEngineAPI {
+    private ComputeAPI computeAPI = new ComputeAPIImpl();
+
+    @Override
+    public int processJob(int n) {
+        return computeAPI.computeSmallestMultiple(n);
+    }
+}

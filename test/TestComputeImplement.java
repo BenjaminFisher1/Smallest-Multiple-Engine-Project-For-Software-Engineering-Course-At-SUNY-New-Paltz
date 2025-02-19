f2c256042bfd1cdb3a7ccd2c7ca09371873bd45e
timkorug1 <timkorug1@newpaltz.edu>
2025-02-19 13:11:46 -0500

    Test Compute Implement

---
+++ test/TestComputeImplement.java
@@ -0,0 +1,17 @@
+package api;
+
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class TestComputeImplement {
+    public void testUserInput(){
+        ComputeImplement impl = mock(ComputeImplement.class);
+        when(impl.userInput(0)).thenReturn(0);
+
+        assertEquals(5,impl.userInputInt(0))
+    }
+}

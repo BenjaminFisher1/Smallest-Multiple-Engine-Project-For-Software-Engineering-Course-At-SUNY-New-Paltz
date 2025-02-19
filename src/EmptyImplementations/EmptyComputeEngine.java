a152e48bf7c5dbe3bd6e227721da3e7259e431a3
Ben Fisher <BenLfish00@gmail.com>
2025-02-19 10:58:24 -0500

    Removed new

--- src/EmptyImplementations/EmptyComputeEngine.java
+++ src/EmptyImplementations/EmptyComputeEngine.java
@@ -5,7 +5,7 @@ public class EmptyComputeEngine extends ComputeEngineAPI{
 		
 		
 	    public ProcessJob processTheJob(ComputeUserInput generalInput) {
-	    	return new ProcessJob();
+	    	return ProcessJob();
 	    	
 	}
 }

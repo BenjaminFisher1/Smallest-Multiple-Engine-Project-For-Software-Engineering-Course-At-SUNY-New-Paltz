57b45616c8241b8699ad777be9229770a7e565a0
Ben Fisher <BenLfish00@gmail.com>
2025-02-24 19:47:17 -0500

    Created the algorithm to solve our problem

---
+++ src/smallestMult.java
@@ -0,0 +1,26 @@
+
+public class smallestMult{
+	public static void main(String[] args) {
+
+		long k = 20;
+		System.out.println(getSmallestMult(k));
+	}
+	
+	public static long getSmallestMult(long n) {
+		long smallMult = 1;
+		long i = 1;
+		while(i <= n) {
+			smallMult = (smallMult * i)/greatestCommonDenom(smallMult, i);
+            i++;
+		}
+		return smallMult;
+	}
+	
+	public static long greatestCommonDenom(long x, long y) {
+		if(y == 0) {
+			return x;
+		} else {
+			return greatestCommonDenom(y, x % y);
+		}
+	}
+}

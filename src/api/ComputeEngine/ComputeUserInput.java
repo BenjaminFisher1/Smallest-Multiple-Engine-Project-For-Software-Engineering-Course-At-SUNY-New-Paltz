3c38513a5583207e598f376a191c9aa6d7b0800c
Ben Fisher <BenLfish00@gmail.com>
2025-02-26 10:59:50 -0500

    Default constructor for computeuserinput

--- src/api/ComputeEngine/ComputeUserInput.java
+++ src/api/ComputeEngine/ComputeUserInput.java
@@ -5,9 +5,13 @@ public class ComputeUserInput {
 	ComputeUserInput(int userInput){		//change later?
 		int value = userInput;
 		
+	ComputeUserInput(){
+		int value = 2;
+	}
+		
 	}
 	public int getValue() {
-		return this.value;
+		return value;
 	}
 	
 }

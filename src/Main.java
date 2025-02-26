77035b3100e591cf03b568068c25581d1f1a85b4
Ben Fisher <BenLfish00@gmail.com>
2025-02-26 17:51:37 -0500

    save this but I am restarting

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main{
 	
 	public static void main(String[] args) {
 		RealNetworker realNetworker = new RealNetworker("sampleInputs/sample");
-		RealDataStorage realDataStorage = new RealDataStorage(realNetworker.request);
+		RealDataStorage realDataStorage = new RealDataStorage(realNetworker);
 		RealComputeEngine realComputeEngine = new RealComputeEngine();
 		
 	}

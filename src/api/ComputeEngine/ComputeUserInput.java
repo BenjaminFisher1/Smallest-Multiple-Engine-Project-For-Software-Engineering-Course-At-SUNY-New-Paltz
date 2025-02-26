bd132623b5e431613dfe34456f09a3298d5cc263
Ben Fisher <BenLfish00@gmail.com>
2025-02-26 11:01:07 -0500

    oops
    fixed construcor

--- src/api/ComputeEngine/ComputeUserInput.java
+++ src/api/ComputeEngine/ComputeUserInput.java
@@ -4,12 +4,12 @@ public class ComputeUserInput {
 		
 	ComputeUserInput(int userInput){		//change later?
 		int value = userInput;
-		
+	}
+	
 	ComputeUserInput(){
 		int value = 2;
 	}
-		
-	}
+	
 	public int getValue() {
 		return value;
 	}

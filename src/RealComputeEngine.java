8fc4dc5949df73354e145c711e040bdb4d180cd6
Ben Fisher <BenLfish00@gmail.com>
2025-02-26 19:23:20 -0500

    removed accidental code

--- src/RealComputeEngine.java
+++ src/RealComputeEngine.java
@@ -6,10 +6,7 @@ public class RealComputeEngine implements ComputeEngineAPI{
 		ProcessedJob processedJob = new ProcessedJob();
 			//runs computation on value of userInput
 	}
-	
-	public ProcessedJob compute(ComputeUserInput userInput) {
-		return new processedJob() = this.computeUponThis(userInput);		//returns a processed job carrying value of computed data	
-	}
+
 	
 	@Override	
 	public ProcessedJob computeUponThis(ComputeUserInput input) {

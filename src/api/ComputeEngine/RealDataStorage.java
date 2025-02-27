c743ed925ce984f56e6f3be6063541630ab3c425
Ben Fisher <BenLfish00@gmail.com>
2025-02-26 22:30:11 -0500

    missing this.

--- src/api/ComputeEngine/RealDataStorage.java
+++ src/api/ComputeEngine/RealDataStorage.java
@@ -18,15 +18,15 @@ public class RealDataStorage implements DataStorage{
 	}
 	
 	public static void storeData(ComputeUserInput userInput) {
-		inputAndOutputMap.put(userInput, new ProcessedJob());			//inserts key and a temp value
+		this.inputAndOutputMap.put(userInput, new ProcessedJob());			//inserts key and a temp value
 	}
 	
 	public void storeData(ComputeUserInput userInput, ProcessedJob processedJob) {			//requires key to replace and value to replace
-		inputAndOutputMap.replace(userInput, processedJob);
+		this.inputAndOutputMap.replace(userInput, processedJob);
 	}
 	
 	public ProcessedJob retrieveData(ComputeUserInput userInput){
-		return inputAndOutputMap.get(userInput);
+		return this.inputAndOutputMap.get(userInput);
 	}
 	
 	public void storeAll(ArrayList<Long> inputs) {

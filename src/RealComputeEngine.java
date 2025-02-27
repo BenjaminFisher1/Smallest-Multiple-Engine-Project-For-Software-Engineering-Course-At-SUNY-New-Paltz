2538f39f3b01355a1259f10e647526a6707785e5
Ben Fisher <BenLfish00@gmail.com>
2025-02-26 19:20:15 -0500

    Overriding computeUponThis

--- src/RealComputeEngine.java
+++ src/RealComputeEngine.java
@@ -11,4 +11,33 @@ public class RealComputeEngine implements ComputeEngineAPI{
 		return new processedJob() = this.computeUponThis(userInput);		//returns a processed job carrying value of computed data	
 	}
 	
+	@Override	
+public static ProcessedJob computeUponThis(ComputeUserInput input) {
+		
+		long returnOutput = getSmallestMult(input.getValue()); //make .value method
+		
+		ProcessedJob returnJob = new ProcessedJob(returnOutput);
+		return returnJob;	
+	
+	}
+	
+	public static long getSmallestMult(int k) {
+		long n = Long.valueOf(k);
+		long smallMult = 1;
+		long i = 1;
+		while(i <= n) {
+			smallMult = (smallMult * i)/greatestCommonDenom(smallMult, i);
+            i++;
+		}
+		return smallMult;
+	}
+	
+	public static long greatestCommonDenom(long x, long y) {
+		if(y == 0) {
+			return x;
+		} else {
+			return greatestCommonDenom(y, x % y);
+		}
+	}
+	
 }

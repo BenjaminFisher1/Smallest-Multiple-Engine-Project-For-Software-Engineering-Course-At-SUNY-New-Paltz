7a32aece194813b7749e17b7c0486dbafe05f368
Ben Fisher <BenLfish00@gmail.com>
2025-02-27 13:57:27 -0500

    Created new calculation test

---
+++ test/TestCalculation.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.Test;
+
+import api.ComputeUserInput;
+import api.ProcessedJob;
+
+
+public class TestCalculation{
+	
+	@Test
+	public void TestCalculation() {
+		ComputeUserInput userInput = mock(ComputeUserInput.class);
+		ProcessedJob proJob = mock(ProcessedJob.class);
+		assertNotEquals(proJob.getValue(), userInput.getValue());
+		
+	}
+	
+}

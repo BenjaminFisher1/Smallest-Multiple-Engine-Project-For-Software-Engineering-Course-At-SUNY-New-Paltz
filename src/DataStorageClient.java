12736b17b9628192c46a6ed231a0e92cb204b11c
Ben Fisher <BenLfish00@gmail.com>
2025-04-13 20:10:08 -0400

    Checkstyle
    Checkstyle

--- src/DataStorageClient.java
+++ src/DataStorageClient.java
@@ -54,7 +54,7 @@ public class DataStorageClient{
 		String target = "localhost:50052";
 		ManagedChannel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create()).build();
 		try {
-			DataStorageClient DSclient = new DataStorageClient(channel);
+			DataStorageClient dSclient = new DataStorageClient(channel);
 		}catch (Exception e){
 			System.out.println("Failed to store");	
 		} finally {

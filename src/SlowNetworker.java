852e88df45713b7e0b1b9e7159ceffd6f1789390
timkorug1 <timkorug1@newpaltz.edu>
2025-04-23 10:34:34 -0400

    Bottleneck (#82)
    * Bottleneck
    
    We used JConsole to find out that there was a bottle neck in this code. The number of threads was making the computations run slower. By increasing the number of threads we elimited said bottleneck.
    
    * Adding SlowNetworker

--- src/SlowNetworker.java
+++ src/SlowNetworker.java
@@ -17,18 +17,18 @@ import api.ComputeUserInput;
 import api.Networker;
 import api.ProcessedJob;
 
-public class MultiNetworker implements Networker{
-	RealComputeEngine realComputeEngine = new RealComputeEngine();
-	RealDataStorage realDataStorage = new RealDataStorage();
-	private int numThreads = 12;
-	private final ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
-	
-	//needs to take user input and give to database
-	
-	public MultiNetworker() {
-		realComputeEngine = new RealComputeEngine();
-		realDataStorage = new RealDataStorage();
-	}
+public class SlowNetworker implements Networker{
+    RealComputeEngine realComputeEngine = new RealComputeEngine();
+    RealDataStorage realDataStorage = new RealDataStorage();
+    private int numThreads = 4;
+    private final ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
+
+    //needs to take user input and give to database
+
+    public SlowNetworker() {
+        realComputeEngine = new RealComputeEngine();
+        realDataStorage = new RealDataStorage();
+    }
 //    public void newFileName(String s){
 //        this.filename = s;
 //    }
@@ -36,66 +36,66 @@ public class MultiNetworker implements Networker{
 
     String defaultFileName = "defualtFile";
 
-	public ArrayList<Long> getList(String filename) {
-		return readIntegers(filename);
-	}
+    public ArrayList<Long> getList(String filename) {
+        return readIntegers(filename);
+    }
 
 
 
-	public ComputeResult compute(ComputeRequest request) throws IOException {
-		ArrayList<Long> tempList = new ArrayList<Long>();
-		
-		RealDataStorage localDataStorage = new RealDataStorage();
-		
+    public ComputeResult compute(ComputeRequest request) throws IOException {
+        ArrayList<Long> tempList = new ArrayList<Long>();
+
+        RealDataStorage localDataStorage = new RealDataStorage();
+
 
         try {
             tempList = (ArrayList<Long>) readIntegers(request.getFileName()).clone();			//maybe revisit if clone is needed
         } catch (Exception e) {
             request.newFileName(defaultFileName); // OR use 'test' if correct
             tempList = (ArrayList<Long>) readIntegers(request.getFileName()).clone();
-		
+
         }
-		
-		localDataStorage.storeAll(tempList);
-		
-		
-	
-		HashMap<ComputeUserInput, ProcessedJob> tempMap = localDataStorage.getInAndOutMap();
-		List<Future<Void>> futures = new ArrayList<>();
-		
-		
-		//need blocking call see playgame use get() on the futures once theyve started see play game example
-		
-		//start threads
-		for(ComputeUserInput input : tempMap.keySet() ) {		//for each key in hash map of database
-			futures.add(threadPool.submit(() ->{
-				
-				localDataStorage.storeData(input, realComputeEngine.computeUponThis(input));	//replace each pair of (input, temp val) with (input, computedInput)
-				return null;
-			}));
-			
-		}
-		//get results from threads (barricade)
-		futures.forEach(future -> {
-			try {
-				future.get();
-			} catch(Exception e) {
-				throw new RuntimeException(e);
-			}
-		});
-		
-		
-		return new ComputeResult(localDataStorage.getOutputFile(request.getOutputFileName()));
-	}
-	
-	
-	
-	
-	
-	
-	
-	//filereading script 
-	
+
+        localDataStorage.storeAll(tempList);
+
+
+
+        HashMap<ComputeUserInput, ProcessedJob> tempMap = localDataStorage.getInAndOutMap();
+        List<Future<Void>> futures = new ArrayList<>();
+
+
+        //need blocking call see playgame use get() on the futures once theyve started see play game example
+
+        //start threads
+        for(ComputeUserInput input : tempMap.keySet() ) {		//for each key in hash map of database
+            futures.add(threadPool.submit(() ->{
+
+                localDataStorage.storeData(input, realComputeEngine.computeUponThis(input));	//replace each pair of (input, temp val) with (input, computedInput)
+                return null;
+            }));
+
+        }
+        //get results from threads (barricade)
+        futures.forEach(future -> {
+            try {
+                future.get();
+            } catch(Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+
+        return new ComputeResult(localDataStorage.getOutputFile(request.getOutputFileName()));
+    }
+
+
+
+
+
+
+
+    //filereading script
+
     public ArrayList<Long> readIntegers(String filePath) {
         ArrayList<Long> integers = new ArrayList<>();
 
@@ -124,7 +124,7 @@ public class MultiNetworker implements Networker{
 
         } catch (FileNotFoundException e) {		//if file not found, we will default to the safetyFile.txt file, that contains simply "1"
             System.out.println("File not found: " + e.getMessage());
-            
+
             try (FileReader reader = new FileReader("safetyFile")) {
                 StringBuilder currentNumber = new StringBuilder();
                 int character;
@@ -149,15 +149,15 @@ public class MultiNetworker implements Networker{
 
             } catch (FileNotFoundException e2) {
                 System.out.println("Missing safety file: " + e.getMessage());
-                
+
             } catch (IOException e2) {
                 System.out.println("Error reading file: " + e.getMessage());
             }
 
             return integers;
-        
-            
-            
+
+
+
         } catch (IOException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
@@ -173,5 +173,5 @@ public class MultiNetworker implements Networker{
             System.out.println("Error writing result to file: " + e.getMessage());
         }
     }
-    
+
 }
